aa77361c504b3df319113df9bf90d5dc87531368
Chen <chenop@gmail.com>
2016-03-13 00:23:31 +0200

    binary tree

---
+++ src/ReverseLinkedList.java
@@ -0,0 +1,44 @@
+/*
+ * To execute Java, please define "static void main" on a class
+ * named Solution.
+ *
+ * If you need more classes, simply define them inline.
+ */
+
+class ReverseLinkedList {
+    public static void main(String[] args) {
+        Node n4 = new Node(5, null);
+        Node n3 = new Node(4, n4);
+        Node n2 = new Node(3, n3);
+        Node n1 = new Node(2, n2);
+
+
+        Node head = n1;
+        while (head != null) {
+            System.out.println(head.data);
+            head = head.next;
+        }
+    }
+
+    public static Node reverse(Node head) {
+        if (head == null || head.next == null ) {
+            return head;
+        }
+
+        Node n1 = head;
+        Node n2 = head.next;
+        n1.next = null;
+
+        while (n2 != null) {
+            Node tmp = n2.next;
+            n2.next = n1;
+            n1 = n2;
+            n2 = tmp;
+        }
+
+        return n1;
+
+    }
+
+
+}
